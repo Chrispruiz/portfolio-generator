a40344a935d2d9f5754eb27c4c7131307c7fbfd5
Chris Ruiz <chrispruiz@att.net>
2021-04-04 22:40:07 -0500

    html generation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-/* const fs = require('fs');
+const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
 const pageHTML = generatePage(name, github);
@@ -9,7 +9,7 @@ fs.writeFile('./index.html', pageHTML, err => {
     if (err) throw err;
 
     console.log('Portfolio complete! Check out index.html to see the output!');
-}); */
+}); 
 
 
 const promptUser = () => {
@@ -145,6 +145,12 @@ const promptUser = () => {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-      console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
   });
   
